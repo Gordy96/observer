2a7e10541f33c7eb6bdc7fcb5182eec331edfa46
hpetrovych <gordy.cp96@gmail.com>
2023-11-08 20:03:59 +0200

    extended test

--- observer_test.go
+++ observer_test.go
@@ -3,6 +3,7 @@ package observer
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestObserve(t *testing.T) {
@@ -10,4 +11,17 @@ func TestObserve(t *testing.T) {
 	assert.NotNil(t, v)
 	assert.Equal(t, 42, v.Get())
 	assert.True(t, v.Is(42))
+
+	ch, cancelCh := v.Subscribe()
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		v.Set(55)
+	}()
+	ex1 := <-ch
+	cancelCh()
+	assert.Equal(t, Transition[int]{42, 55}, ex1)
+	ch, cancelCh = v.Subscribe()
+	cancelCh()
+	_, ok := <-ch
+	assert.False(t, ok)
 }
